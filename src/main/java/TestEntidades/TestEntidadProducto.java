c6417e4ba2faf7b073c713c4e3f8f52c4e756395
diego <sotelocordovadiego@gmail.com>
2024-07-03 03:01:49 -0500

    segundo

---
+++ src/main/java/TestEntidades/TestEntidadProducto.java
@@ -0,0 +1,32 @@
+package TestEntidades;
+
+import java.util.List;
+
+import Dao.ClassProductoImp;
+import model.TblProductocl3;
+
+public class TestEntidadProducto {
+
+	public static void main(String[] args) {
+		TblProductocl3 producto = new TblProductocl3();
+        ClassProductoImp crud = new ClassProductoImp();
+        /*
+        producto.setNombrecl3("Jabon");
+        producto.setPrecioventacl3(3.50);
+        producto.setPreciocompcl3(1.50);
+        producto.setEstadocl3("Nuevo");
+        producto.setDescripcl3("Lavanda");
+        crud.RegistrarProducto(producto);
+        */
+        
+        List<TblProductocl3> listarProducto = crud.ListarProducto();
+        for(TblProductocl3 p:listarProducto){
+            System.out.println("Código: "+p.getIdproductoscl3()+"\n"+
+                                "Nombre: "+p.getNombrecl3()+"\n"+
+                                "Precio de Venta: "+p.getPrecioventacl3()+"\n"+
+                                "Precio de Compra: "+p.getPreciocompcl3()+"\n"+
+                                "Estado: "+p.getEstadocl3()+"\n"+
+                                "Descripción: "+p.getDescripcl3()+"\n");
+        }   
+	}
+}
